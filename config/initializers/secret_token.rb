936617501aa49e828bfda67b8b9e6ceeaa58f325
Steven Burdsall <sdsb110@gmail.com>
2014-02-26 22:12:08 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+APNSS::Application.config.secret_key_base = '6b5967e5dd83c34d70937e90c36fa62cfa8808a33c3ef04201eecb0fa4721cbde583d75c60372d432ef91f4079dc23baada94cd22585e5727c40a32ae4c64477'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+APNSS::Application.config.session_store :cookie_store, key: '_APNSS_session'
